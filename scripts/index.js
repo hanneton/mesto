a58f698e601775355973040865092418b7ef0925
Anton Rumyantsev <blush3d@ya.ru>
2023-02-09 03:25:40 +0300

    feature:to add cards through js

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,48 @@ function saveInfo(e) {
 btnEdit.addEventListener('click', showPopup);
 btnClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', saveInfo);
+
+/* 6 cards feature */
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
+
+function addCard(src, name) {
+    let elementsContainer = document.querySelector('.elements');
+    let cardTemplate = document.querySelector('#card').content;
+    let cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__name').textContent = name;
+    cardElement.querySelector('.element__pic').src = src;
+    elementsContainer.append(cardElement);
+}
+
+initialCards.forEach(el => {
+    addCard(el.link, el.name);
+})
